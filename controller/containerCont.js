a0c498f03408176caa448739395dc91899f8f17d
ade8m <benammaradem83@gmail.com>
2023-05-08 22:34:09 +0100

    create fisrt container use dockerode

--- controller/containerCont.js
+++ controller/containerCont.js
@@ -1,23 +1,41 @@
 const Docker = require('dockerode');
+const docker1 = new Docker({ socketPath: '/var/run/docker.sock' });
 const docker = new Docker();
 
 exports.createContainer = (req, res) => {
-  const containerConfig = {
-    ImgName: req.body.ImgName,
-    ContainerName: req.body.containerName,
-    cmd: ['nginx', '-g', 'daemon off;'],
-    Tty: true,
-    HostConfig: {
-      PortBindings: {
-        '80/tcp': [
-          {
-            Port: req.body.portNumber,
-          },
-        ],
-      },
-    },
+  const containerOptions = {
+    Image: 'nginx:latest',
+    Cmd: [],
+    AttachStdin: false,
+    AttachStdout: true,
+    AttachStderr: true,
+    Tty: false,
+    OpenStdin: false,
+    StdinOnce: false,
+    
   };
+  docker.createContainer(containerOptions, (err, container) => {
+    if (err) {
+      console.error('Error creating container:', err);
+      return;
+    }
   
+    // Container created successfully
+    console.log('Container created:', container.id);
+      // Start the container
+  container.start((err) => {
+    if (err) {
+      console.error('Error starting container:', err);
+      return;
+    }
+
+    console.log('Container started:', container.id);
+  });
+});
+
+  
+  
+ /* 
   docker.createContainer(containerConfig).then((container) => {
     console.log(`Container created with ID ${container.id}`);
     // Start container
@@ -28,5 +46,5 @@ exports.createContainer = (req, res) => {
   }).catch((err) => {
     console.error(err);
     res.status(500).send('Error creating container');
-  });
+  });*/
 };
